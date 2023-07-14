82a1b441d628649ef36d1c591615ff2a8d7309c0
Pablo Ferrari <pabloferraricaliari@gmail.com>
2023-07-14 16:35:52 -0300

    update invalid character error message

--- src/errors/invalid-character-error.ts
+++ src/errors/invalid-character-error.ts
@@ -1,6 +1,6 @@
 export class InvalidCharacterError extends Error {
   constructor(character: string) {
-    super(`input-mask-react error: Charactere inválido: '${character}'`)
+    super(`input-mask-react error: Invalid haractere: '${character}'`)
     this.name = 'InvalidCharacterError'
   }
 }
